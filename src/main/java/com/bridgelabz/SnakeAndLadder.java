3528d2623dc028c102b45deeeff5bf293f40c7aa
datta1808 <bagaledatta18@gmail.com>
2021-05-20 16:22:43 +0530

    ADDED: functionality to get a Random Number on Dice Roll

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,13 +1,19 @@
 package com.bridgelabz;
+import java.util.Random;
 
 public class SnakeAndLadder {
 
     public static final int STARTING_POSITION = 0;
     public static final int NUMBER_OF_PLAYERS = 1;
 
+    public static int rollDice(){
+        Random random = new Random();
+        int diceValue = random.nextInt(6)+1;
+        return diceValue;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
-        System.out.println("Starting Position: " +STARTING_POSITION);
-        System.out.println("Number of Players: " +NUMBER_OF_PLAYERS);
+        System.out.println("Dice Number = " +rollDice());
     }
 }
