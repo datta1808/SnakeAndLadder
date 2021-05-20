1d6c89d1710b24be733991c3a98d36bc60aff339
datta1808 <bagaledatta18@gmail.com>
2021-05-20 15:43:14 +0530

    ADDED: Starting Position and Number of Players

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+
+    public static final int STARTING_POSITION = 0;
+    public static final int NUMBER_OF_PLAYERS = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
+        System.out.println("Starting Position: " +STARTING_POSITION);
+        System.out.println("Number of Players: " +NUMBER_OF_PLAYERS);
     }
 }
