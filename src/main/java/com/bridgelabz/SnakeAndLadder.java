1b4fbfb43ee21b317abcffb861cf91eac031ea8c
datta1808 <bagaledatta18@gmail.com>
2021-05-20 22:05:40 +0530

    ADDED: functionality to check the player move

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -5,15 +5,48 @@ public class SnakeAndLadder {
 
     public static final int STARTING_POSITION = 0;
     public static final int NUMBER_OF_PLAYERS = 1;
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    //initialized the user position with start position
+    static int PLAYER_POSITION = STARTING_POSITION;
 
+    // method to get the value of dice
     public static int rollDice(){
         Random random = new Random();
         int diceValue = random.nextInt(6)+1;
         return diceValue;
     }
 
+    // method to get the value of moves
+    public static int moves(){
+        Random random = new Random();
+        int moveValue = random.nextInt(3);
+        return moveValue;
+    }
+
+    //Method to set the position of the player
+    public static void setPlayerPosition(int playerPosition) {
+        SnakeAndLadder.PLAYER_POSITION = playerPosition;
+    }
+
+    //Method to get the position of the player
+    public static int getPlayerPosition() {
+        return PLAYER_POSITION;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Game");
-        System.out.println("Dice Number = " +rollDice());
+
+        int numberOnDice = rollDice();
+        int playerMove = moves();
+
+        if(playerMove == NO_PLAY) {
+          setPlayerPosition(getPlayerPosition());
+        } else if(playerMove == LADDER) {
+            setPlayerPosition(getPlayerPosition() + rollDice());
+        } else if(playerMove == SNAKE) {
+            setPlayerPosition(getPlayerPosition() - rollDice());
+        }
+        System.out.println(getPlayerPosition());
     }
 }
